ed25a20930bfa1aad7892ab92d26b2f9888ac615
Depriest43 <depriest43@gmail.com>
2023-12-01 23:01:47 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsiClassPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }//Main
+    }//Class
+    //Here is where I would write the code to make a Class.
+}//NameSpace
